0a1d13c5d9dd4d8dfc256db9e34dbee38659db72
MandarAcharekar <mandarmva8055@gmail.com>
2024-07-15 00:50:22 +0530

    error handling for all calls added

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require('node:fs');
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -26,32 +26,43 @@ app.get("/", (req, res) => {
 
 // GET /tasks: Retrieve all tasks.
 app.get("/tasks", (req, res) => {
-    return res.send(tasks);
+    try{
+        return res.send(tasks);
+    }
+    catch(error){
+        return res.status(500).json({"msg": `Error occured : ${error}`})
+    }
 });
 
 // GET /tasks/:id: Retrieve a single task by its ID.
 app.get("/tasks/:id", (req, res) => {
-    const id = req.params.id;
-
-    for(let i=0;i<tasks.length;i++){
-        if(tasks[i]["id"] == id){
-            return res.send(tasks[i])
-        }
-    }// for
-
-    return res.status(404).send({"msg": `Task with id=${id} not found`});
+    try{
+        const id = req.params.id;
+    
+        for(let i=0;i<tasks.length;i++){
+            if(tasks[i]["id"] == id){
+                return res.send(tasks[i])
+            }
+        }// for
+    
+        return res.status(404).send({"msg": `Task with id=${id} not found`});
+    }
+    catch(error){
+        return res.status(500).json({"msg": `Error occured : ${error}`})
+    }
 });
 
 // POST /tasks: Create a new task.
 app.post("/tasks/", (req, res) => {
-    const title = req.body.title;
-    const description = req.body.description;
-    const completed = req.body.completed;
-
-    if(typeof title == 'undefined' || typeof description == 'undefined' || typeof completed == 'undefined'){
-        return res.status(400).send({"msg": "Payload is incorrect"});
-    }
-    else{
+    try{
+        const title = req.body.title;
+        const description = req.body.description;
+        const completed = req.body.completed;
+        
+        if(typeof title == 'undefined' || typeof description == 'undefined' || typeof completed == 'undefined'){
+            return res.status(400).send({"msg": "Payload is incorrect"});
+        }
+     
         const newTask = {
             id: tasks.length + 1,
             title: title,
@@ -59,58 +70,65 @@ app.post("/tasks/", (req, res) => {
             completed: completed
         };
         tasks.push(newTask);
-
-        // latestTask = tasks.slice(-1);
-        return res.status(201).json({
-            title: title,
-            description: description,
-            completed: completed
-        });
+    
+        return res.status(201).json(tasks.slice(-1));
+    }
+    catch(error){
+        return res.status(500).json({"msg": `Error occured : ${error}`})
     }
 });
 
 // PUT /tasks/:id: Update an existing task by its ID.
 app.put("/tasks/:id", (req, res) => {
-    const id = req.params.id;
-
-    const title = req.body.title;
-    const description = req.body.description;
-    const completed = req.body.completed;
-
-    if(typeof title !== 'string' || typeof description !== 'string' || typeof completed !== 'boolean'){
-        return res.status(400).send({"msg": "Payload is incorrect"});
-    }
-
-    for(let i=0;i<tasks.length;i++){
-        if(tasks[i]["id"] == id){
-            tasks[i].title = title;
-            tasks[i].description = description;
-            tasks[i].completed = completed;
-
-            return res.send(tasks[i]);
+    try{
+        const id = req.params.id;
+    
+        const title = req.body.title;
+        const description = req.body.description;
+        const completed = req.body.completed;
+    
+        if(typeof title !== 'string' || typeof description !== 'string' || typeof completed !== 'boolean'){
+            return res.status(400).send({"msg": "Payload is incorrect"});
         }
+    
+        for(let i=0;i<tasks.length;i++){
+            if(tasks[i]["id"] == id){
+                tasks[i].title = title;
+                tasks[i].description = description;
+                tasks[i].completed = completed;
+    
+                return res.send(tasks[i]);
+            }
+        }
+    
+        return res.status(404).send({"msg": `Task with id=${id} not found`});
     }
-
-    return res.status(404).send({"msg": `Task with id=${id} not found`});
+    catch(error){
+        return res.status(500).json({"msg": `Error occured : ${error}`})
+    }
 });
 
 // DELETE /tasks/:id: Delete a task by its ID.
 app.delete("/tasks/:id", (req, res) => {
-    const id = req.params.id;
-    let index = null;
-
-    for(let i=0;i<tasks.length;i++){
-        if(tasks[i]["id"] == id){
-            index = i;
+    try{
+        const id = req.params.id;
+        let index = null;
+    
+        for(let i=0;i<tasks.length;i++){
+            if(tasks[i]["id"] == id){
+                index = i;
+            }
         }
-    }
-
-    if(typeof index === 'number'){
-        const deletedTask = tasks.splice(index, 1)[0]
-        return res.send(deletedTask);
-    }
-    else{
+    
+        if(typeof index === 'number'){
+            const deletedTask = tasks.splice(index, 1)[0];
+            return res.send(deletedTask);
+        }
+        
         return res.status(404).send({"msg": `Task with id=${id} not found`});
+    }
+    catch(error){
+        return res.status(500).json({"msg": `Error occured : ${error}`})
     }
 });
 
